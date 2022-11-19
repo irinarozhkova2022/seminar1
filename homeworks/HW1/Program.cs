be29187e958cf98aa600ea94e738c52d77dd782d
Irina <irina-rozhkova-87@mail.ru>
2022-11-19 21:08:11 +0300

    Zadacha4

--- homeworks/HW1/Program.cs
+++ homeworks/HW1/Program.cs
@@ -1,5 +1,33 @@
-﻿Console.WriteLine("Input number 1");
+﻿/*Console.WriteLine("Input number 1");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number 2");
 int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num1 > num2 ? "Первое число больше второго" : "Второе число больше первого");
+Console.WriteLine(num1 > num2 ? "Первое число больше второго" : "Второе число больше первого");*/
+
+Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 3");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("Максимальное число: " + num1);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число: " + num3);
+    }
+}
+
+else if (num2 > num3)
+{
+    Console.WriteLine("Максимальное число: " + num2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + num3);
+}
