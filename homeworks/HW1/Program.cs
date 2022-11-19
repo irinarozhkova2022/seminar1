d38f586b893d0514d71d05457f8dd914246dc362
Irina <irina-rozhkova-87@mail.ru>
2022-11-19 20:50:54 +0300

    Zadacha2

--- homeworks/HW1/Program.cs
+++ homeworks/HW1/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input number 1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number 2");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num1 > num2 ? "Первое число больше второго" : "Второе число больше первого");
